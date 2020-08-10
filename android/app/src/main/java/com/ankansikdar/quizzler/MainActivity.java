97a01707467be93a4c6db5f6b4c6aaed334e04c9
Ankan Sikdar <ankansikdar@gmail.com>
2020-08-10 13:08:34 +0530

    Initial Commit

---
+++ android/app/src/main/java/com/ankansikdar/quizzler/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ankansikdar.quizzler;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Quizzler());
+}
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+      ),
+    );
+  }
+}
